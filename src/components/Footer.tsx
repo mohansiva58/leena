2bf87e52041f9b4cb546e9d6e5bbb2876c79e543
T. Mohan Siva <mohansivathota@gmail.com>
2026-05-25 22:45:57 +0530

    feat: simplify Footer component by removing unnecessary markup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,7 @@
-
-
-import React from "react";
-import "./ui/footer.css";
 import Footercard from "./ui/Footercard";
+
 export function Footer() {
   return (
-    <>
-      <Footercard />
-
-      <footer className="new_footer_area mb-16 lg:mb-0">
-        <div className="new_footer_top">
-          <div className="footer_bg">
-            <div className="footer_bg_one"></div>
-            <div className="footer_bg_two"></div>
-            <div className="footer_bg_three"></div>
-          </div>
-        </div>
-      </footer>
-    </>
+    <Footercard />
   );
 }
